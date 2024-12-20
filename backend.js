28e824b1c6e293cbbf808731396a1fba3189201d
CashewMan <95064541+CashewMan430@users.noreply.github.com>
2024-12-20 15:28:51 -0600

    Updates

--- backend.js
+++ backend.js
@@ -1,1 +1,24 @@
-//Test
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'anishka.loves.aakaash@gmail.com',
+    pass: 'Iloveu09202024'
+  }
+});
+
+var mailOptions = {
+  from: 'anishka.loves.aakaash@gmail.com',
+  to: 'aak.jamad@gmail.com',
+  subject: 'I love you',
+  text: 'Hi baby, Anishka here. I just wanted to say I love you. Mmmmmmmmmmmwaa.'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
