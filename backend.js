27093b62b6d00fed1f36c1052c6c007cf8a43d2e
CashewMan <95064541+CashewMan430@users.noreply.github.com>
2024-12-20 19:28:37 -0600

    Bug fixes

--- backend.js
+++ backend.js
@@ -10,7 +10,7 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-const PORT = process.env.PORT || 2009;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
