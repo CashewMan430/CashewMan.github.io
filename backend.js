0d348c1d0d6493df57faeb6b5c8a846202b4baee
CashewMan <95064541+CashewMan430@users.noreply.github.com>
2024-12-20 18:50:23 -0600

    Updates

--- backend.js
+++ backend.js
@@ -12,7 +12,7 @@ var transporter = nodemailer.createTransport({
 
 const PORT = process.env.PORT || 2009;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.post('/send-email', (req, res) => {
